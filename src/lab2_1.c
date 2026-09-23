e9c7d7223a5925fe24e3218457f301a25185e742
nicatjafarli101-rgb <nicatjafarli101@gmail.com>
2026-09-23 10:58:46 +0300

    Add sum_to_n function and main program

---
+++ src/lab2_1.c
@@ -0,0 +1,19 @@
+#include
+int sum_to_n(int n) {
+int sum = 0;
+for (int i = 1; i <= n; i++) {
+sum += i;
+}
+return sum;
+}
+int main(void) {
+int n;
+printf("Enter n: ");
+scanf("%d", &n);
+if (n < 1) {
+printf("Error: n must be at least 1.\n");
+} else {
+printf("Sum from 1 to %d = %d\n", n, sum_to_n(n));
+}
+return 0;
+}
